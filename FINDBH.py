bee805a18a84ce8a6c160cd5303e7cb530a33ee5
AstroCom <astrocom@Davids-MBP-3.internal.amnh.org>
2018-06-13 12:36:56 -0400

    new

--- FINDBH.py
+++ FINDBH.py
@@ -1,44 +1,30 @@
 
 # coding: utf-8
 
-# In[ ]:
+# assignement: Find Black Hole 
+# 
 
-
-Assignement: find black hole
-
-
-# In[4]:
+# In[20]:
 
 
 import pynbody
-import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+import os
+pd.set_option('display.max_column', None)
+pd.set_option('display.max_rows', None)
+import gc
+get_ipython().magic(u'matplotlib inline')
 
-#im loading the snapshot                                                                   
 
-s = pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
-
-#save it by ctrl-x-s 'to save work'                                                        
+# In[ ]:
 
 
-# gonna start loading the halo'galaxies' now                                               
-
+s = pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')   
+s.physical_units()
+ pynbody.plot.image(h5.g, width=100, cmap='Blues');
 h = s.halos()
-
-#halos we need to only look at # 5                                                         
-
-
-pynbody.analysis.angmom.faceon(h[5])
-
-#^^ its centering on the halo that i have picked which is 3 at the moment                  
-#and giving it a shape of a disk almost like a dinner plate                                
-
-s.physical_units()#^^ starts everything from the center reaching outwards almost like axis 
-
-# find the BH in the snapshot
-def findBH (s):
-    BHfilter = pynbody.filt.LowPass('tform',0.0)
-    BH = snap.stars[BHfilter]
-    return BH
-
-pynbody.analysis.halo.center(h[5],mode='hyb')
+h5= h[5]
+pynbody.analysis.halo.center(h5,mode='hyb')
+print(h[5]['pos'][0])
 
