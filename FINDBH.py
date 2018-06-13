7c91b46b163eaa43505419d7f8f9bbdacf7931f2
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-13 14:51:18 -0400

    Update FINDBH.py

--- FINDBH.py
+++ FINDBH.py
@@ -12,13 +12,16 @@ import numpy as np
 import pandas as pd
 
 s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')   
-s.physical_units()
+h = s.halos()
 pynbody.analysis.angmom.faceon(h[5])
-h[5]= s.halos()
+s.physical_units()
+
+
 
 def findBH(h5):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
     h[5]= s.stars[BHfilter]
     return h[5]
 
-
+p = pynbody.analysis.profile.Profile(h[5].s,min=.01,max=50)
+f, axs = plt.subplots(figsize=(14,6))# me changing it to make one plot
