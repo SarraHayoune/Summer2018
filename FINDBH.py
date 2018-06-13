e7899805e6464108e07e8a0f36d3b59deafaee2c
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-13 13:04:57 -0400

    Update FINDBH.py

--- FINDBH.py
+++ FINDBH.py
@@ -24,14 +24,6 @@ def findBHhalos(h5):
     BH = findBH(h5)
     BHhalos = BH['amiga.grp']
     return BHhalos
-for i in sortedhaloinds:
-        # which halo are we on?  need to center 
-        currenthalo = BHhalos[i]
-        print 'current halo: ',currenthalo
-        if currenthalo != halo:  # need to center on new halo
-            print "new halo calcs"
-            halo = currenthalo
-            pynbody.analysis.halo.center(h[currenthalo],mode='ssc')
 
 data = [BHhalos[i],BH['pos'][i].in_units('kpc'),getz(s),gettime(s),BH['r'][i]]
 info = pd.DataFrame(data,columns=columns)
