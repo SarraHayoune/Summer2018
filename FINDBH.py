d2319d5d73b74f60e53218c1b14658b15e41871d
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-13 12:57:20 -0400

    Update FINDBH.py

--- FINDBH.py
+++ FINDBH.py
@@ -14,13 +14,13 @@ import pandas as pd
 s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')   
 s.physical_units()
 h = s.halos()
-def findBH(H[5]):
+def findBH(h[5]):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
     BH = snap.stars[BHfilter]
     return BH
 
-def findBHhalos(H[5]):
-    BH = findBH(snap)
+def findBHhalos(h[5]):
+    BH = findBH(h[5])
     BHhalos = BH['amiga.grp']
     return BHhalos
  for i in sortedhaloinds:
