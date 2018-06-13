a4ef54ff6042973fda7d4e95dd5a27c0588f58e7
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-13 14:20:37 -0400

    Update FINDBH.py

--- FINDBH.py
+++ FINDBH.py
@@ -10,17 +10,11 @@
 import pynbody
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_column', None)
-pd.set_option('display.max_rows', None)
 
 s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')   
 s.physical_units()
 h = s.halos()
 h5= h[5]
-
-# initialize dataframe
-columns = ['mass','haloid','BHpos','BHvel','redshift','time','bhiord','halodist','Mvir','Mstar','Mgas']
-bhinfo = pd.DataFrame(columns=columns)
 
 def findBH(h5):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
