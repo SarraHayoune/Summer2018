f11bc4eb90c667d811854cc2589d95f1f0cdb2bc
Sarra <sarra@winnipeg.internal.amnh.org>
2018-06-14 16:45:35 -0400

    ununderstoodoutput

--- FINDBH.py
+++ FINDBH.py
@@ -1,37 +1,30 @@
-# assignement: Find Black Hole
-
-
+   # assignement: Find Black Hole
 import pynbody 
-import numpy as np 
-import pandas as pd 
-pd.set_option('display.max_column', None)
-pd.set_option('display.max_rows', None)
+import numpy as np
+import pandas as pd
+  
+  # loading the snapshot
+s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
 
-s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096') 
-h = s.halos() 
-pynbody.analysis.angmom.faceon(s) 
-p = pynbody.analysis.profile.Profile(h[5].s,min=.01,max=50,ndim=3) 
-pynbody.analysis.halo.center(h[5],mode='hyb') 
-s.physical_units() 
+  # the halo that I need is h[5]
+h = s.halos()
+h5=h[5]
+
+  # put your galaxy that you care about in the center of the simulation
+pynbody.analysis.angmom.faceon(h5)
+
+  # convert the units 
+s.physical_units()
+print (h[5]['pos'][0])
+print (h[5]['pos'][1])
+print (h[5]['pos'][2])
 
 def findBH(s):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
     BH = s.stars[BHfilter]
-    return B
+    return BH
+BH = findBH(s)
 
-grpid=1
-w=80;
+BH=BH['pos']
 
-pynbody.analysis.halo.center(h[grpid],mode='ssc');
-pynbody.analysis.angmom.faceon(h[grpid], cen=(0,0,0))
-
-BH=s.star[pynbody.filt.BandPass('tform','-15 Gyr','0 Gyr')]
-BH_pos=BH['pos']
-nBH=len(BH['iord']);
-
-h[grpid].s['tform'].convert_units('Gyr');
-
-pynbody.plot.image(h[grpid].s,qty="tform",cmap="Greys", width=w, log=False);
-plt.plot(np.array((BH[np.where(BH['amiga.grp']==grpid)]['pos'])).T[0],np.array((BH[np.where(BH['amiga.grp']==grpid)]['pos'])).T[1],'r+')
-plt.xlim(-w/2,w/2)
-plt.ylim(-w/2,w/2);
+ 
