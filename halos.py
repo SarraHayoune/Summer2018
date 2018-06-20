a76198fba06c7344e1a8ada2668307ad714893ed
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-20 16:34:09 -0400

    Update halos.py

--- halos.py
+++ halos.py
@@ -25,10 +25,33 @@ def findBH(s):
     return BH
 BH = findBH(s)
 print BH
+
 def findBHhalos(s):
     BH = findBH(s)
     BHhalos = BH['amiga.grp']
     return BHhalos
 halos = findBHhalos(s)
 print halos
+
+for halo in halos
+    
+    #the position of black hole
+   BHposition=BH['pos']
+   print BHposition
+
+   #putting the x-values into a column
+   BHx= BHposition[:,0]
+   print BHx
+
+   #putting the y-values into a column
+   BHy= BHposition[:,1]
+   print BHy
+
+   #putting the z-values into a column
+   BHz= BHposition[:,2]
+   print BHz
+
+   #this is the distance formula
+   distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+   print distance
 
