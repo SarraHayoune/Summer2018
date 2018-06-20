c641b05e54ee0aa67f3e8fee060a2c00b65879b6
AstroCom <astrocom@Davids-MBP-3.internal.amnh.org>
2018-06-20 14:22:21 -0400

    py code

---
+++ halos.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# Assignement2" FIND ALL THE BLACK HOLES "
+
+# In[ ]:
+
+
+import pynbody 
+import matplotlib.pylab as plt
+plt.switch_backend("agg") 
+
+   # loading the snapshot
+s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
+
+   # convert the units 
+s.physical_units()
+
+  # the halo that I need is h[5]
+h = s.halos()
+
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH[0,1,2]
+BH = findBH(s)
+
