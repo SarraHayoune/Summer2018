9167cbcfe182e5d0d39beca99ea9a97444b60b3a
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-22 16:28:28 -0400

    Update halos.py

--- halos.py
+++ halos.py
@@ -38,22 +38,22 @@ for halo in halos:
     print halos
     
     #the position of black hole
-   BHposition=BH['pos']
-   print BHposition
+    BHposition=BH['pos']
+    print BHposition
 
    #putting the x-values into a column
-   BHx= BHposition[:,0]
-   print BHx
+    BHx= BHposition[:,0]
+    print BHx
 
    #putting the y-values into a column
-   BHy= BHposition[:,1]
-   print BHy
+    BHy= BHposition[:,1]
+    print BHy
 
    #putting the z-values into a column
-   BHz= BHposition[:,2]
-   print BHz
+    BHz= BHposition[:,2]
+    print BHz
 
    #this is the distance formula
-   distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
-   print distance
+    distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+    print distance
 
