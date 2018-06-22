2db1579f2b34b45add432beb05e8bfa9335cabbd
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-22 16:24:47 -0400

    Update halos.py

--- halos.py
+++ halos.py
@@ -24,9 +24,9 @@ for halo in halos:
     s.physical_units()
    
     with pynbody.analysis.halo.center(h[5], mode='hyb'):
-    print (h[halo]['pos'][0])
-    print (h[halo]['pos'][1])
-    print (h[halo]['pos'][2])
+         print (h[halo]['pos'][0])
+         print (h[halo]['pos'][1])
+         print (h[halo]['pos'][2])
    
    # function to find black hole
     def findBH(s):
