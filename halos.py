bef038d391ac5b396a6e69ec91d468e00f48136e
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-25 14:18:59 -0400

    Update halos.py

--- halos.py
+++ halos.py
@@ -6,44 +6,42 @@ import matplotlib.pylab as plt
 
  # loading the snapshot
 s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
- 
-   # convert the units 
-s.physical_units()
-# the halo that I need is h[5]
-h = s.halos()
- # function to find black hole
-def findBH(s):
-    BHfilter = pynbody.filt.LowPass('tform',0.0)
-    BH = s.stars[BHfilter]
-    return BH
-BH = findBH(s)
-print BH
 
-def findBHhalos(s):
-    BH = findBH(s)
-    BHhalos = BH['amiga.grp']
-    return BHhalos
-halos = findBHhalos(s)
-print halos 
-    
-for halo in h:
+for halo in s:
+    s.physical_units()
+# the halo that I need is h[5]
+    h = s.halos()
     print 'current halo:', halo
-  
+   
+ # function to find black hole
+    def findBH(s):
+        BHfilter = pynbody.filt.LowPass('tform',0.0)
+        BH = s.stars[BHfilter]
+        return BH
+    BH = findBH(s)
+    print BH
+
+    def findBHhalos(s):
+        BH = findBH(s)
+        BHhalos = BH['amiga.grp']
+        return BHhalos
+    halos = findBHhalos(s)
+    print halos 
+
     # put your galaxy that you care about in the center of the simulation
 
-     pynbody.analysis.angmom.faceon(h[halo])
+    pynbody.analysis.angmom.faceon(h[halo])
    # convert the units 
-       s.physical_units()
+    s.physical_units()
    # the halo that I need is h[5]
-     halo = 0  # initialize what halo we are on
    
-     with pynbody.analysis.halo.center(h[halo], mode='hyb'):
-             print (h[halo]['pos'][0])
-             print (h[halo]['pos'][1])
-             print (h[halo]['pos'][2])
+    with pynbody.analysis.halo.center(h[halo], mode='hyb'):
+         print (h[halo]['pos'][0])
+         print (h[halo]['pos'][1])
+         print (h[halo]['pos'][2])
     #this is the distance formula
-     distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
-     print distance
+    distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+    print distance
    
       
    
