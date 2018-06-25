ea2267ffd630fe8112c527e8fa1aad92d14a3afd
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-25 13:58:50 -0400

    Update halos.py

--- halos.py
+++ halos.py
@@ -27,6 +27,7 @@ halos = findBHhalos(s)
 print halos 
     
 for halo in halos:
+    print 'current halo:', halo
     # put your galaxy that you care about in the center of the simulation
     pynbody.analysis.angmom.faceon(h[halo])
    # convert the units 
@@ -40,34 +41,14 @@ for halo in halos:
     print halos[sortedhaloinds]
     halo = 0  # initialize what halo we are on
    
-    for i in sortedhaloinds:
-        # which halo are we on?  need to center 
-        currenthalo = halos[i]
-        print 'current halo: ',currenthalo
-        if currenthalo != halo:  # need to center on new halo
-            print "new halo calcs"
-            halo = currenthalo
-           
-        with pynbody.analysis.halo.center(h[halo], mode='hyb'):
-             print (h[halo]['pos'][0])
-             print (h[halo]['pos'][1])
-             print (h[halo]['pos'][2])
+       
+    with pynbody.analysis.halo.center(h[halo], mode='hyb'):
+          print (h[halo]['pos'][0])
+          print (h[halo]['pos'][1])
+          print (h[halo]['pos'][2])
+    #this is the distance formula
+    distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+    print distance
       
-    
-    #the position of black hole
-        BHposition=BH['pos']
-        print BHposition
-
-   #putting the x-values into a column
-        BHx= BHposition[:,0]
-        print BHx
-
-   #putting the y-values into a column
-        BHy= BHposition[:,1]
-        print BHy
-
-   #putting the z-values into a column
-        BHz= BHposition[:,2]
-        print BHz
- 
+   
   
