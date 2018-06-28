5fa6f465a7b6d14dac65c72910169cb11d91d3f1
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-06-28 16:05:42 -0400

    final code to find all the BH

--- halos.py
+++ halos.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pylab as plt
 
 # loading the snapshot
-s = pynbody.load('/mnt/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096')
+s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
 
 # convert the units 
 s.physical_units()
@@ -36,33 +36,27 @@ for i in currenthalo:
     # which halo are we on?  
     currenthalo= halos[i]
     print 'current halo: ', currenthalo
-    if currenthalo != currenthalo:  # need to center on new halo
-       print currenthalo
-     
-       
+ 
+ 
        # put your galaxy that you care about in the center of the simulation
       
-       pynbody.analysis.angmom.faceon(h[currenthalo])
-       with pynbody.analysis.halo.center(h[currenthalo], mode='hyb'):
+    pynbody.analysis.angmom.faceon(h[currenthalo])
+    with pynbody.analysis.halo.center(h[currenthalo], mode='hyb'):
      
     # the position of black hole
-            BHposition=BH['pos']
-            print BHposition
+         BHposition=BH['pos']
+         print BHposition
       
    # x-values 
-            BHx= BHposition[i,0]
+         BHx= BHposition[i,0]
     
    # y-values 
-            BHy= BHposition[i,1]  
+         BHy= BHposition[i,1]  
     
    # z-values
-            BHz= BHposition[i,2]
+         BHz= BHposition[i,2]
       
     #this is the distance formula
-            distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
-            print 'distance:', distance
-   
-   
-
-   
+         distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+         print 'distance:', distance
    
