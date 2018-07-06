d527deda74d2486cd922c0320f1da01141917969
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-06 18:08:57 -0400

    Update bhfile.py

--- bhfile.py
+++ bhfile.py
@@ -88,5 +88,5 @@ for i in currenthalo:
     print "this is the distance :", distance
 
     data = [[BH['mass'][i],BHhalos[i],BH['pos'][i].in_units('kpc'),BH['vel'][i],getz(s),gettime(s),BH['iord'][i],BH['r'][i]]
-    f.write(data)
+    write(data)
     close(f)
