8123ab4375c3a1f2f3da05171e79a1ce6338c597
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-09 14:49:46 -0400

    Update grossery+list.py

--- grossery+list.py
+++ grossery+list.py
@@ -7,10 +7,10 @@
 import pandas as pd
 
 
-f= open("list.txt","w")
-data = ["banana","apple","potato", "tomato"]
-f.write("data")
-f= open("list.txt","r")
+f= open("list.txt","a+")
+#data = ["banana","apple","potato", "tomato"]
+f.write("data,"banana")
+#f= open("list.txt","r")
 
 print f.read()
 f.close()
