265eb99ac5f3d8f94e13fb22b7587c339985ec32
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-09 14:07:05 -0400

    Update grossery+list.py

--- grossery+list.py
+++ grossery+list.py
@@ -6,8 +6,7 @@
 
 import pandas as pd
 f= open("list.txt","w")
-data = [['banana',1],['apple',2],['fraise',3]]
-df = pd.DataFrame(data,columns=['Name','Q'])
+data = ['banana','apple','fraise']
 f.write(data)
 f.close()
 #print df
