1a8a17d1fc48393eb2885a2e56ea828f629bf6f3
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-10 15:41:05 -0400

    Update grossery+list.py

--- grossery+list.py
+++ grossery+list.py
@@ -8,16 +8,20 @@ import pandas as pd
 
 
 f= open("list.txt","w")
-data = ["banana","apple","potato", "tomato"]
+data = [1,2,3,4,"banana","apple","potato", "tomato"]
 f.write(str(data))
 f= open("list.txt","r")
+for eachitem in data:
+    f.write(str(eachitem)+'\n')
 
-list = f.readline()
-while list:
-      data= list.split()
-      f.write(data+'\n')
-      print (data)
-      list = f.readline()
+
+
+#list = f.readline()
+#while list:
+      #data= list.split()
+      #f.write(data+'\n')
+      #print (data)
+      #list = f.readline()
 f.close()
 #print df
 
