79d8c317adbaa50414b5f375c4ac130b9dfeaef4
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-18 11:19:59 -0400

    add velocity

--- bhfile.py
+++ bhfile.py
@@ -83,7 +83,7 @@ for i in currenthalo:
     distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
     #print 'this is the distance :'
     print "this is the distance :", distance
-    data = [currenthalo, BH['iord'][i], gettime(s),getz(s), BH['velovity'][i], BH['mass'][i], BH['r'][i]] 
+    data = [currenthalo, BH['iord'][i], gettime(s),getz(s), BH['vel'][i], BH['mass'][i], BH['r'][i]] 
     f.write(str(data)+'\n')
     #f= open("bhfile.txt","w+")
     #for j in data:
