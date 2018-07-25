e61cf841474701c2336cb0400d40c75a9c9141fb
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-25 14:45:39 -0400

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -18,16 +18,16 @@ Time= files[:,2]
 BHDistance= files[:,5]
 
 ID=[89422247, 89422741, 89425759]
-i1= np.where(89422247)
+i1= np.where(ID==89422247)
 #i2= np.where(ID== 89422741)
 #i3= np.where(ID== 89425759)
 
 
 #BHDistance.("]","")
 #np.char.strip(BHDistance)
-Time= np.array(Time)
+Time= np.array(Time[i1])
 
-BHDistance=np.array(BHDistance)
+BHDistance=np.array(BHDistance[i1])
 
 
 print Time
@@ -37,8 +37,8 @@ plt.plot(Time[i1], BHDistance[i1])
 #plt.plot(Time[i2], BHDistance[i2])
 #plt.plot(Time[i3], BHDistance[i3])
 
-plt.ylabel('BH Distance')
-plt.xlabel('Time')
+plt.ylabel('BH Distance[i1]')
+plt.xlabel('Time[i1]')
 plt.show()
 
 
