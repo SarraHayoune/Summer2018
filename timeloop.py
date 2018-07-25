746ce4f094415d30f83e5db58aa26608c236d22d
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-25 11:11:23 -0400

    loading all the snaps(38)

--- timeloop.py
+++ timeloop.py
@@ -7,17 +7,8 @@ import matplotlib.pylab as plt
 import readcol
 
 
-files = readcol.readcol('bhfile.dat')
+files = readcol.readcol('/mnt/cptmarvel/files.list')
 files = files[:,0]
-
-# loading the snapshot
-s =pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
-
-# convert the units 
-s.physical_units()
-
-#  load any available halo
-h = s.halos()
 
 # function to find black hole
 def findBH(s):
@@ -37,21 +28,30 @@ def findBHhalos(s):
 BHhalos = findBHhalos(s)
 print BHhalos
 
-#sorting the halos, indexes/indecis are like an exact address
-currenthalo = np.argsort(BHhalos)
-print BHhalos[currenthalo]
 
 def getz(s):
     return s.properties['z']
 
 def gettime(s):
     return pynbody.analysis.cosmology.age(s)
-# initialize dataframe
-#columns = ['BHpos','BHvel','redshift','time','bhiord','halodist']
-#bhinfo = pd.DataFrame(columns=columns)
 
-#f =  open("bhfile.dat", "w+") 
+f =  open("bhfile.dat", "w+") 
 for file in files:
+    
+    # loading the snapshotS
+    s =pynbody.load('file')
+ 
+    # convert the units 
+    s.physical_units()
+
+    #  load any available halo
+    h = s.halos()
+    BH = findBH(s)
+    BHhalos = findBHhalos(s)
+    #sorting the halos, indexes/indecis are like an exact address
+    currenthalo = np.argsort(BHhalos)
+    print BHhalos[currenthalo]
+
     for i in currenthalo:
     
         #which halo are we on?
@@ -62,17 +62,14 @@ for file in files:
         #put the galaxy you care about in the center of the simulation
         pynbody.analysis.angmom.faceon(h[currenthalo])
 
-    #this is the position of black hole
+        #this is the position of black hole
         BHposition=BH['pos']
 
-    #printing the position of black hole
-    #print BHposition
-
-    #putting the x-values into a column
+        #putting the x-values into a column
         BHx= BHposition[[i],0]
         print "x postion", BHx
    
-    #putting the y-values into a column
+        #putting the y-values into a column
         BHy= BHposition[[i],1]
         print "y position", BHy
 
@@ -80,22 +77,13 @@ for file in files:
         BHz= BHposition[[i],2]
         print "z position", BHz
 
-    #the .5 is the square root , this is the distance formula
+        #the .5 is the square root , this is the distance formula
         distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
-    #print 'this is the distance :'
-        print "this is the distance :", distance
+        print "the distance is:", distance
     
-    #starmass = currenthalo.s['mass']
-    #gasmass = currenthalo.g['mass']
-    #virialmass = starmass+gasmass+currenthalo.d['mass']
         data = [currenthalo, BH['iord'][i], gettime(s),getz(s), BH['mass'][i], BH['r'][i]] 
-        #f.write(str(data)+'\n')
-    #f= open("bhfile.txt","w+")
-    #for j in data:
-        #f= open("bhfile.dat","w+")
-       # info = pd.DataFrame(data,columns=columns)
-       # bhinfo = bhinfo.append(info)
-        #f.write(str(i))
+        f.write(str(data)+'\n')
+    
     print data
          
 f.close()
