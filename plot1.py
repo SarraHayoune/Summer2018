4755d8d18da825c6ef320f1ccf3a886cf0382c10
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-25 14:38:35 -0400

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -13,8 +13,16 @@ import readcol
 
 
 files = readcol.readcol('bhfile.dat',fsep=',')
+BHID= files[:,1]
 Time= files[:,2]
 BHDistance= files[:,5]
+
+ID=[89422247, 89422741, 89425759]
+i1= np.where(ID== 89422247)
+i2= np.where(ID== 89422741)
+i3= np.where(ID== 89425759)
+
+
 #BHDistance.("]","")
 #np.char.strip(BHDistance)
 Time= np.array(Time)
@@ -25,7 +33,9 @@ BHDistance=np.array(BHDistance)
 print Time
 print BHDistance
 
-plt.plot(Time, BHDistance)
+plt.plot(Time[i1], BHDistance[i1])
+plt.plot(Time[i2], BHDistance[i2])
+plt.plot(Time[i3], BHDistance[i3])
 
 plt.ylabel('BH Distance')
 plt.xlabel('Time')
