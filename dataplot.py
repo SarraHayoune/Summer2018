02880aafd3478385ac7c41521fcad3e5cc156373
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-30 14:27:56 -0400

    plot all the black holes

--- dataplot.py
+++ dataplot.py
@@ -17,12 +17,16 @@ BHID= files[:,1]
 Time= files[:,2]
 BHDistance= files[:,5]
 
-#ID=[89422247, 89422741, 89425759]
+ID=[89422247, 89422741, 89425759]
 
-#i1= np.where(BHID==307622464)
-i2= np.where(BHID== 227839049)
-#i3= np.where(BHID== 89425759)
-
+i1= np.where(BHID==89422247)
+i2= np.where(BHID== 89422741)
+i3= np.where(BHID== 89425759)
+i4= np.where(BHID== 243770516)
+i5= np.where(BHID== 243778457)
+i6= np.where(BHID== 243771992)
+i7= np.where(BHID== 307622464)
+i8= np.where(BHID== 227839049)
 
 #BHDistance.("]","")
 #np.char.strip(BHDistance)
@@ -34,9 +38,14 @@ i2= np.where(BHID== 227839049)
 #print Time
 #print BHDistance
 
-#plt.plot(Time[i1], BHDistance[i1],  label= "black hole")
-plt.plot(Time[i2], BHDistance[i2], label= "black hole")
-#plt.plot(Time[i3], BHDistance[i3], label= "black hole 5")
+plt.plot(Time[i1], BHDistance[i1],  label= "black hole 11")
+plt.plot(Time[i2], BHDistance[i2], label= "black hole 10")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 5")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 16")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 2a")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 2b")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 1")
+plt.plot(Time[i3], BHDistance[i3], label= "black hole 3")
 
 plt.ylabel("BH Distance 'Kpc'")
 plt.xlabel("Time 'Gyr'")
