2626139826bd95e22b0db793360f2c525ba698fa
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-07-31 15:30:34 -0400

    make it bold

--- histogramplot.py
+++ histogramplot.py
@@ -6,6 +6,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from Tkinter import *
+
+
+
 #get_ipython().magic(u'matplotlib inline')
 
 x = np.arange(5)
@@ -16,8 +20,13 @@ plt.xticks(x+.25, [ 0,1,2,3,4])
 plt.yticks(x+1)
 
 
-plt.xlabel('distance from halo center (Kpc)', size = 14)
-plt.ylabel('BH Number', size= 14)
+plt.xlabel('distance from halo center (Kpc)',size= 18 )
+plt.ylabel('BH Number',   size= 18)
+main = Tk()
+
+Label(main, 
+		 text="Red Text in Times Font",
+		 fg = "red",
+		 font = "times 18 bold italic").pack()
 #plt.legend()
 plt.show('plot2.png')
-
