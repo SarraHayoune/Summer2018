5a28885fe9801d77099c2ab01f3c23c2c71c770b
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-08-01 22:03:55 -0400

    Update histogramplot.py

--- histogramplot.py
+++ histogramplot.py
@@ -6,7 +6,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from Tkinter import *
+
 
 
 
@@ -20,13 +20,9 @@ plt.xticks(x+.25, [ 0,1,2,3,4])
 plt.yticks(x+1)
 
 
-plt.xlabel('distance from halo center (Kpc)',size= 18 )
-plt.ylabel('BH Number',   size= 18)
-main = Tk()
+plt.xlabel('distance from halo center (Kpc)',size= 20 )
+plt.ylabel('BH Number',   size= 20)
 
-Label(main, 
-		 text="Red Text in Times Font",
-		 fg = "red",
-		 font = "times 18 bold italic").pack()
+
 #plt.legend()
 plt.show('plot2.png')
