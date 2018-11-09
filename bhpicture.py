9f4fc234d996d8eb499809e78357ab5f38ff1d18
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-11-09 13:33:24 -0500

    Update bhpicture.py

--- bhpicture.py
+++ bhpicture.py
@@ -33,6 +33,27 @@ BHhalos = findBHhalos(s)
  #sorting the halos, indexes/indecis are like an exact address
 halolist = np.argsort(BHhalos)
 #print BHhalos[currenthalo]
+def plot_BH_pos(h, BH, attribute, cmap='Greys', w=100, lc='best',save=False, filename='plots/dg_adjTime/h%s_t%s_w%s.png'):
+    '''plots position of black holes. Send halo array(h[grpid]), black hole array(BH),attribute 
+    Optional: width/preferred scope(w) (default=100), colormap(default='Greys'), save(default=False)'''
+    
+    halo_id=int(np.unique(h['amiga.grp']))
+    time=h.properties['time'].in_units('Gyr')
+    
+    pynbody.plot.image(h,qty=attribute,cmap=cmap, width=w, log=False)
+    for i in np.where(BH['amiga.grp']==halo_id):
+        plt.plot(np.array(BH[i]['pos']).T[0],np.array(BH[i]['pos']).T[1],'r+', label=BH[i]['iord'])
+    plt.xlim(-w/2,w/2)
+    plt.ylim(-w/2,w/2)
+    plt.title('Halo %s at %s Gyr'%(halo_id, round(time, 1)))
+    plt.legend(fancybox=True, loc='lc')
+    plt.tight_layout()
+
+    if save==True:
+        plt.savefig(filename%(halo_id, round(time, 1), w),bbox_inches='tight', dpi=200)
+
+        
+
 
 for i in halolist:
     
@@ -63,8 +84,8 @@ for i in halolist:
     BHz= BHposition[[i],2]
     print "z position", BHz
     # create an image using  the default bands (i, v, u)
-    plt.plot(BHx, BHy,'+') 
-    pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo'+str(currenthalo)+'.png')
-    plt.plot(BHx, BHy,'+')
+    #plt.plot(BHx, BHy,'+') 
+    #pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo'+str(currenthalo)+'.png')
+    #plt.plot(BHx, BHy,'+')
                         
-
+    plot_BH_pos(h[currenthalo].s, BH,'tform', w=20, save=True)
