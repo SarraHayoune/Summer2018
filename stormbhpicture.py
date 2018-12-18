ae22abda1cbece0381e7b7fd441e5b6942a99cc6
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-12-18 13:23:38 -0500

    Update stormbhpicture.py

--- stormbhpicture.py
+++ stormbhpicture.py
@@ -1,10 +1,12 @@
 import pynbody
 import matplotlib.pylab as plt
 import numpy as np
+import BH_FUNCTIONS.py as BHF
 
 # load the snapshot and set to physical units
 
-s= pynbody.load('/mnt/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096')
+#s= pynbody.load('/mnt/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096')
+s= pynbody.load('/jillian/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096')
 
 # convert the units
 s.physical_units()
@@ -35,37 +37,54 @@ halolist = np.argsort(BHhalos)
 
 
 
-for i in halolist:
+#for i in halolist:
     
         #which halo are we on?
-    currenthalo = BHhalos[i]
-    print 'current halo: ', currenthalo
-    print i
+currenthalo = BHhalos[h[2]]
+print 'current halo: ', currenthalo
+    #print i
     
 
  # center on the largest halo and aligh the disk
       
-    pynbody.analysis.halo.center(h[currenthalo], mode= 'hyb');
+    #pynbody.analysis.halo.center(h[2], mode= 'hyb');
+pynbody.analysis.angmom.faceon(h[2])
         
        
 
         #this is the position of black hole
-    BHposition=BH['pos']
+ BHposition=BH['pos']
 
         #putting the x-values into a column
-    BHx= BHposition[[i],0]
-    print "x postion", BHx
+ BHx1= BHposition[[0],0]
+ BHx2= BHposition[[1],0]
+    #print "x postion", BHx
    
         #putting the y-values into a column
-    BHy= BHposition[[i],1]
-    print "y position", BHy
+ BHy1= BHposition[[0],1]
+ BHy2= BHposition[[1],1]
+    #print "y position", BHy
 
          #putting the z-values into a column
-    BHz= BHposition[[i],2]
-    print "z position", BHz
-    # create an image using  the default bands (i, v, u)
+ BHz1= BHposition[[0],2]
+ BHz2= BHposition[[1],2]
+    #print "z position", BHz
+   
     #plt.plot(BHx, BHy,'+') 
-    #pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo'+str(currenthalo)+'.png')
-    #plt.plot(BHx, BHy,'+')
+   # create an image using  the default bands (i, v, u)
+  #pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo'+str(h2)+'.png')
+  BHF.render(s,width= '5 kpc', filename='stormbhpicture.png')
+  plt.plot(BHx1, BHy1,'+')
+  plt.plot(BHx2, BHy2,'+')
+  plt.savefig('stormpicture.png')
+   
+    # center on the largest halo and aligh the disk
+ 
+#pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='haloFaceon.png')
+# center on the largest halo and aligh the disk
+#pynbody.analysis.angmom.sideon(h[5])
+# create an image using  the default bands (i, v, u)
+#pynbody.plot.stars.render(s,width= '10 kpc',plot=True,ret_im=True,filename='halo11edgeon.png')
                         
-    plot_BH_pos(h[currenthalo].s, BH,'tform', w=20, save=True)
+ #plot_BH_pos(h[2].s, BH,'tform', w=20, save=True)
+ 
