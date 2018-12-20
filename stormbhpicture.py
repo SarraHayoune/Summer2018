9d39a4d555e84dda8ff79fa49b64d6fa3e01a7bd
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2018-12-20 16:46:21 -0500

    Updated manually

--- stormbhpicture.py
+++ stormbhpicture.py
@@ -29,22 +29,6 @@ def findBHhalos (s):
     return BHhalos
 
 
-BHhalos = findBHhalos(s)
-
- #sorting the halos, indexes/indecis are like an exact address
-halolist = np.argsort(BHhalos)
-#print BHhalos[currenthalo]
-
-
-
-#for i in halolist:
-    
-        #which halo are we on?
-currenthalo = BHhalos[h[2]]
-print 'current halo: ', currenthalo
-    #print i
-    
-
  # center on the largest halo and aligh the disk
       
     #pynbody.analysis.halo.center(h[2], mode= 'hyb');
@@ -56,35 +40,26 @@ pynbody.analysis.angmom.faceon(h[2])
  BHposition=BH['pos']
 
         #putting the x-values into a column
- BHx1= BHposition[[0],0]
- BHx2= BHposition[[1],0]
+ BHx1= BHposition[[1],0]
+ BHx2= BHposition[[2],0]
     #print "x postion", BHx
    
         #putting the y-values into a column
- BHy1= BHposition[[0],1]
- BHy2= BHposition[[1],1]
-    #print "y position", BHy
+ BHy1= BHposition[[1],1]
+ BHy2= BHposition[[2],1]
+    #print "y positon", BHy
 
          #putting the z-values into a column
- BHz1= BHposition[[0],2]
- BHz2= BHposition[[1],2]
+ BHz1= BHposition[[1],2]
+ BHz2= BHposition[[2],2]
     #print "z position", BHz
    
     #plt.plot(BHx, BHy,'+') 
    # create an image using  the default bands (i, v, u)
   #pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo'+str(h2)+'.png')
-  BHF.render(s,width= '5 kpc', filename='stormbhpicture.png')
+  BHF.render(s,width= '25 kpc', plot= Tue, ret_im= True, filename='stormbhpictures.png')
   plt.plot(BHx1, BHy1,'+')
   plt.plot(BHx2, BHy2,'+')
-  plt.savefig('stormpicture.png')
+  plt.savefig('stormpicturefaceon.png')
    
-    # center on the largest halo and aligh the disk
- 
-#pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='haloFaceon.png')
-# center on the largest halo and aligh the disk
-#pynbody.analysis.angmom.sideon(h[5])
-# create an image using  the default bands (i, v, u)
-#pynbody.plot.stars.render(s,width= '10 kpc',plot=True,ret_im=True,filename='halo11edgeon.png')
-                        
- #plot_BH_pos(h[2].s, BH,'tform', w=20, save=True)
- 
+    
